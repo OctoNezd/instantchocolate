4929eb706ebdd52e139c084854efffd744ddd08a
Yan <me@octonezd.me>
2021-04-21 17:30:08 +0300

    Load packageData from argument

--- createFuseIndex.js
+++ createFuseIndex.js
@@ -1,13 +1,10 @@
-const axios = require("axios");
 const fuse = require("fuse.js");
 const fs = require("fs");
-const axiosInstance = axios.create({ baseURL: process.env.VUE_APP_API_URL });
-(async () => {
-  const packageData = (await axiosInstance.get("package_data.json")).data
-    .software;
-  const index = fuse.createIndex(
-    ["packageName", "displayName", "summary", "tags"],
-    packageData
-  );
-  fs.writeFileSync("fuse-index.json", JSON.stringify(index.toJSON()));
-})();
+const packageData = JSON.parse(
+  fs.readFileSync(process.argv[process.argv.length - 1])
+).software;
+const index = fuse.createIndex(
+  ["packageName", "displayName", "summary", "tags"],
+  packageData
+);
+fs.writeFileSync("fuse-index.json", JSON.stringify(index.toJSON()));
